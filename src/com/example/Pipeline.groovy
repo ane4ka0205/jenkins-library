6d756a160eb894208b3d16580cc6f3c0b715c7b6
Anna Malanova <anna.malanova@slalom.com>
2019-10-21 16:07:20 -0500

    change3

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -1,9 +1,4 @@
 package com.example
 
-stage('Git Checkout') {
-    gitCheckout(
-        branch: "master",
-        url: "https://github.com/ane4ka0205/mybase-project.git"
-    )
-}
-
+def printHello() {
+    sh 'echo "hello world from library"'
