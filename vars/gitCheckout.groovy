3d7c8da6d65b4bd39d026c6fef4d894684d4228a
Anna Malanova <anna.malanova@slalom.com>
2019-10-21 18:44:29 -0500

    change5

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,9 +1,27 @@
-def printHello() {
-    sh 'echo "hello world from library"'
+// def printHello() {
+//     sh 'echo "hello world from library"'
+// }
+
+// def printBye() {
+//   sh 'echo "Bye"'
+// }
+
+def setCommand(commandToRun) {
+  cmd = commandToRun
 }
 
-def printBye() {
-  sh 'echo "Bye"'
+def getCommand() {
+  cmd
+}
+
+def runCommand() {
+  timestamps {
+    cmdOut = sh (script:"${cmd}", returnStdout:true).trim()
+  }
+}
+
+def getOutput() {
+  cmdOut
 }
 
 
