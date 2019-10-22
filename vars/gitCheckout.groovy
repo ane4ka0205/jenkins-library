a6e9951ea76f508e239b793325293d4e1f866cb0
Anna Malanova <anna.malanova@slalom.com>
2019-10-21 19:09:39 -0500

    change4

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -6,7 +6,15 @@
 //   sh 'echo "Bye"'
 // }
 
-
+class buildUtils {
+    static def timedGradleBuild(script,tasks) {
+        def gradleHome = script.tool 'gradle3.2'
+        script.sh "echo Building for ${script.env.BUILD_TAG}"
+        script.timestamps {
+            script.sh "${gradleHome}/bin/gradle ${tasks}"
+        }
+    }
+}
 
 
     
